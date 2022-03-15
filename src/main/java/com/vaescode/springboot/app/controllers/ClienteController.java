9ff85fcade8be0020871213f252a61a257569557
Cesar-Dev <cesarvargasescorcia@gmail.com>
2022-03-15 08:47:14 -0600

    CONTROLADOR: handler listar clientes

---
+++ src/main/java/com/vaescode/springboot/app/controllers/ClienteController.java
@@ -0,0 +1,24 @@
+package com.vaescode.springboot.app.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.vaescode.springboot.app.models.dao.IClienteDao;
+
+@Controller
+public class ClienteController {
+
+	@Autowired
+	private IClienteDao clienteDao;
+	
+	@GetMapping(value = "/listar")
+	public String listar(Model model) {
+
+		model.addAttribute("titulo", "Listado de clientes");
+		model.addAttribute("cliente", clienteDao.findAll());
+
+		return "listar";
+	}
+}
