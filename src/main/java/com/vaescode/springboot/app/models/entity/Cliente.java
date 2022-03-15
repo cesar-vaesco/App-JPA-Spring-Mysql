003d6a3b6e12ba9ae53a93476fe53e47e2a8a882
Cesar-Dev <cesarvargasescorcia@gmail.com>
2022-03-15 08:31:48 -0600

    @Entity: Clase entidad JPA Cliente

---
+++ src/main/java/com/vaescode/springboot/app/models/entity/Cliente.java
@@ -0,0 +1,73 @@
+package com.vaescode.springboot.app.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente implements Serializable {
+
+	@Id // Indica que es la llave primaria
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String nombre;
+	private String apellido;
+	private String correo;
+
+	@Column(name = "create_at")
+	@Temporal(TemporalType.DATE) // Como se guarda la fecha en la base de datos
+	private Date createAt;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public Date getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+
+	private static final long serialVersionUID = 1L;
+
+}
