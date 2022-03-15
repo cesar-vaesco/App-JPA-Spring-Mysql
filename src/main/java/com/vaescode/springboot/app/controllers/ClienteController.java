5cb8c0652cdc1c582c993f2091c42791a6bb790b
Cesar-Dev <cesarvargasescorcia@gmail.com>
2022-03-15 13:20:53 -0600

    Validaciones: agregando reglas-anotaciones de validación en clase Usuario y mostrando mensajes de error en la vista form

--- src/main/java/com/vaescode/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/vaescode/springboot/app/controllers/ClienteController.java
@@ -2,9 +2,13 @@ package com.vaescode.springboot.app.controllers;
 
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -36,7 +40,12 @@ public class ClienteController {
 	}
 
 	@PostMapping("/form")
-	public String guardar(Cliente cliente) {
+	public String guardar(@Valid Cliente cliente, BindingResult result, Model model) {
+		
+		if(result.hasErrors()) {
+			model.addAttribute("titulo", "Formulario de clientes");
+			return "form";
+		}
 		clienteDao.save(cliente);
 		return "redirect:listar";
 	}
--- src/main/java/com/vaescode/springboot/app/models/entity/Cliente.java
+++ src/main/java/com/vaescode/springboot/app/models/entity/Cliente.java
@@ -12,6 +12,10 @@ import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -23,10 +27,18 @@ public class Cliente implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	private String nombre;
+	@NotEmpty
+	@Size(min = 4, max = 12)
+ 	private String nombre;
+	
+	@NotEmpty
 	private String apellido;
+	
+	@NotEmpty
+	@Email
 	private String email;
 
+	@NotNull
 	@Column(name = "create_at")
 	@Temporal(TemporalType.DATE) // Como se guarda la fecha en la base de datos
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
