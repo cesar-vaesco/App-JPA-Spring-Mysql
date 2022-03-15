82b9c96e2043b3444bc9649e8dc04eb402258a80
Cesar-Dev <cesarvargasescorcia@gmail.com>
2022-03-15 09:55:57 -0600

    Controlador: métodos para cargar y guardar formulario

--- src/main/java/com/vaescode/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/vaescode/springboot/app/controllers/ClienteController.java
@@ -1,18 +1,22 @@
 package com.vaescode.springboot.app.controllers;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.vaescode.springboot.app.models.dao.IClienteDao;
+import com.vaescode.springboot.app.models.entity.Cliente;
 
 @Controller
 public class ClienteController {
 
 	@Autowired
 	private IClienteDao clienteDao;
-	
+
 	@GetMapping(value = "/listar")
 	public String listar(Model model) {
 
@@ -21,4 +25,20 @@ public class ClienteController {
 
 		return "listar";
 	}
+
+	@GetMapping("/form")
+	public String crear(Map<String, Object> model) {
+
+		Cliente cliente = new Cliente();
+		model.put("titulo", "Formulario de cliente");
+		model.put("cliente", cliente);
+		return "form";
+	}
+
+	@PostMapping("/form")
+	public String guardar(Cliente cliente) {
+		clienteDao.save(cliente);
+		return "redirect:listar";
+	}
+
 }
--- src/main/java/com/vaescode/springboot/app/models/dao/IClienteDao.java
+++ src/main/java/com/vaescode/springboot/app/models/dao/IClienteDao.java
@@ -7,5 +7,7 @@ import com.vaescode.springboot.app.models.entity.Cliente;
 public interface IClienteDao {
 	
 	public List<Cliente> findAll();
+	
+	public void save(Cliente cliente);
 
 }
