8460afcdf2fe6d22c3d17c464a11f58646cd1ea7
Cesar-Dev <cesarvargasescorcia@gmail.com>
2022-03-15 12:53:13 -0600

    Formulario: Agregando fecha - fijando formato

--- src/main/java/com/vaescode/springboot/app/models/entity/Cliente.java
+++ src/main/java/com/vaescode/springboot/app/models/entity/Cliente.java
@@ -13,6 +13,8 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 @Entity
 @Table(name = "clientes")
 public class Cliente implements Serializable {
@@ -27,12 +29,9 @@ public class Cliente implements Serializable {
 
 	@Column(name = "create_at")
 	@Temporal(TemporalType.DATE) // Como se guarda la fecha en la base de datos
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date createAt;
 
-	@PrePersist
-	public void prePersist() {
-		createAt = new Date();
-	}
 
 	public Long getId() {
 		return id;
