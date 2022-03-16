59ec5a25f8c781120d4964a428a38431d69244a8
Cesar-Dev <cesarvargasescorcia@gmail.com>
2022-03-16 14:00:27 -0600

    Carga de imagenes: agregando campo file en formulario y procesar multifile en el controlador

--- src/main/java/com/vaescode/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/vaescode/springboot/app/controllers/ClienteController.java
@@ -1,5 +1,9 @@
 package com.vaescode.springboot.app.controllers;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
 
 import javax.validation.Valid;
@@ -19,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.vaescode.springboot.app.models.entity.Cliente;
@@ -31,27 +36,24 @@ public class ClienteController {
 
 	@Autowired
 	private IClienteService clienteService;
-	
-	
-	
+
 	/* listar */
 	@RequestMapping(value = "/listar", method = RequestMethod.GET)
-	public String listar(@RequestParam(name = "page", defaultValue = "0") int page,  Model model) {
-		
-		Pageable pageRequest = PageRequest.of(page, 4); //cantidad registros a mostrar por página
-		
+	public String listar(@RequestParam(name = "page", defaultValue = "0") int page, Model model) {
+
+		Pageable pageRequest = PageRequest.of(page, 4); // cantidad registros a mostrar por página
+
 		Page<Cliente> clientes = clienteService.findAll(pageRequest);// Obtenemos la lista página de registros
-		
+
 		PageRender<Cliente> pageRender = new PageRender<>("/listar", clientes);
 
 		model.addAttribute("titulo", "Listado de clientes");
-		model.addAttribute("clientes",clientes); //pasar lista a la vista 
-		model.addAttribute("page",pageRender); //pasar paginación a la vista
+		model.addAttribute("clientes", clientes); // pasar lista a la vista
+		model.addAttribute("page", pageRender); // pasar paginación a la vista
 
 		return "listar";
 	}
-	
-	
+
 	/* crear registro de cliente */
 	@GetMapping("/form")
 	public String crear(Map<String, Object> model) {
@@ -62,7 +64,6 @@ public class ClienteController {
 		return "form";
 	}
 
-	
 	/* editar registro de cliente */
 	@GetMapping("/form/{id}")
 	public String editar(@PathVariable(value = "id") Long id, Map<String, Object> model, RedirectAttributes flash) {
@@ -71,10 +72,10 @@ public class ClienteController {
 
 		if (id > 0) {
 			cliente = clienteService.findOne(id);
-			if(cliente == null) {
+			if (cliente == null) {
 				flash.addFlashAttribute("error", "El Id del cliente no existe en la base de datos");
 				return "redirect:/listar";
-				
+
 			}
 		} else {
 			flash.addFlashAttribute("error", "El Id del cliente no puede ser cero!");
@@ -87,27 +88,41 @@ public class ClienteController {
 		return "form";
 	}
 
-	
-	/*Enviar registro de cliente a base de datos*/
+	/* Enviar registro de cliente a base de datos */
 	@PostMapping("/form")
-	public String guardar(@Valid Cliente cliente, BindingResult result, Model model, RedirectAttributes flash, SessionStatus status) {
+	public String guardar(@Valid Cliente cliente, BindingResult result, Model model,
+			@RequestParam("file") MultipartFile foto, RedirectAttributes flash, SessionStatus status) {
 
 		if (result.hasErrors()) {
 			model.addAttribute("titulo", "Formulario de clientes");
 			return "form";
 		}
-		
-		String mensajeFlash = (cliente.getId() != null)? "Cliente editado con éxito!" : "Cliente creado con éxito!";
-		
-		
+
+		if (!foto.isEmpty()) {
+			Path directorioRecursos = Paths.get("src//main//resources//static//upload");
+			String rootPath = directorioRecursos.toFile().getAbsolutePath();
+			try {
+				byte[] bytes = foto.getBytes();
+				Path rutaCompleta = Paths.get(rootPath + "//" + foto.getOriginalFilename());
+				Files.write(rutaCompleta, bytes);
+				flash.addFlashAttribute("info", "ha subido correctamente '" + foto.getOriginalFilename()+ "'");
+				cliente.setFoto(foto.getOriginalFilename());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		String mensajeFlash = (cliente.getId() != null) ? "Cliente editado con éxito!" : "Cliente creado con éxito!";
+
 		clienteService.save(cliente);
 		status.setComplete();
 		flash.addFlashAttribute("success", mensajeFlash);
 		return "redirect:/listar";
 	}
 
-	/*Eliminar registro de cliente*/
-	@RequestMapping(value="/eliminar/{id}")
+	/* Eliminar registro de cliente */
+	@RequestMapping(value = "/eliminar/{id}")
 	public String eliminar(@PathVariable(value = "id") Long id, RedirectAttributes flash) {
 		if (id > 0) {
 			clienteService.delete(id);
