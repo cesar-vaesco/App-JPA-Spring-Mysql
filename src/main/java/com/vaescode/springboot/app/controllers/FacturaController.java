70a9f62b304cfcd39c783ae01de857d2d6ffa6d0
Cesar-Dev <cesarvargasescorcia@gmail.com>
2022-03-17 14:51:06 -0600

    SISTEMA FACTURACION: controlador de factura - handler crear factura

---
+++ src/main/java/com/vaescode/springboot/app/controllers/FacturaController.java
@@ -0,0 +1,49 @@
+package com.vaescode.springboot.app.controllers;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.vaescode.springboot.app.models.entity.Cliente;
+import com.vaescode.springboot.app.models.entity.Factura;
+import com.vaescode.springboot.app.models.service.IClienteService;
+
+@Controller
+@RequestMapping("/factura")
+@SessionAttributes("factura")
+public class FacturaController {
+
+	@Autowired
+	private IClienteService clienteService;
+
+	@GetMapping("/form/{clienteId}")
+	public String crear(@PathVariable(value = "clienteId") Long clienteId, Map<String, Object> model,
+			RedirectAttributes flash) {
+
+		// Se busca el cliente
+		Cliente cliente = clienteService.findOne(clienteId);
+
+		// En caso de no encontrar el cliente
+		if (cliente == null) {
+			flash.addFlashAttribute("error", "El cliente no existe en la base de datos");
+			return "redirect:/listar";
+		}
+
+		// Si se encuentra el cliente se crea una instancia de factura
+		Factura factura = new Factura();
+		factura.setCliente(cliente); // Asignación de un cliente a una factura
+
+		// pasar información a la vista
+		model.put("factura", factura);
+		model.put("titulo", "Crear Factura");
+
+		return "factura/form";
+	}
+
+}
