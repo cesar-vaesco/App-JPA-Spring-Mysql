4b725b0dacf2974ef1464ebbc009b88024b1889b
Cesar-Dev <cesarvargasescorcia@gmail.com>
2022-03-23 23:01:20 -0600

    SECURITY: método configure para dar persmiso de acceso a los usuarios

--- src/main/java/com/vaescode/springboot/app/SpringSecurityConfig.java
+++ src/main/java/com/vaescode/springboot/app/SpringSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
@@ -20,15 +21,23 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 		return new BCryptPasswordEncoder();
 	}
 
+	/* ACL - Listas de control de acceso - peemisos para acceder a las vistas */
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/", "/css/**", "/js/**", "/images/**", "/listar*").permitAll()
+				.antMatchers("/ver/**").hasAnyRole("USER").antMatchers("/uploads/**").hasAnyRole("USER")
+				.antMatchers("/form/**").hasAnyRole("ADMIN").antMatchers("/eliminar/**").hasAnyRole("ADMIN")
+				.antMatchers("/factura/**").hasAnyRole("ADMIN").anyRequest().authenticated();
+	}
+
 	@Autowired
 	public void configurerGlobal(AuthenticationManagerBuilder builder) throws Exception {
 		PasswordEncoder encoder = passwordEncoder();
-		UserBuilder users = User.builder().passwordEncoder(encoder :: encode);
-		
-		builder.inMemoryAuthentication()
-		.withUser(users.username("admin").password("12345").roles("ADMIN","USER"))
-		.withUser(users.username("cesar").password("12345").roles("USER"));
-		
+		UserBuilder users = User.builder().passwordEncoder(encoder::encode);
+
+		builder.inMemoryAuthentication().withUser(users.username("admin").password("12345").roles("ADMIN", "USER"))
+				.withUser(users.username("cesar").password("12345").roles("USER"));
+
 		/*
 		 * UserBuilder users = User.builder().passwordEncoder(password -> { return
 		 * encoder.encode(password); });
--- src/main/java/com/vaescode/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/vaescode/springboot/app/controllers/ClienteController.java
@@ -76,7 +76,7 @@ public class ClienteController {
 	}
 
 	/* listar */
-	@RequestMapping(value = "/listar", method = RequestMethod.GET)
+	@RequestMapping(value = {"/listar","/"}, method = RequestMethod.GET)
 	public String listar(@RequestParam(name = "page", defaultValue = "0") int page, Model model) {
 
 		Pageable pageRequest = PageRequest.of(page, 4); // cantidad registros a mostrar por página
