a8d76442c54491611dd4af0cbf519a1a0bc43286
Cesar-Dev <cesarvargasescorcia@gmail.com>
2022-03-25 20:38:38 -0600

    LOCALE y MULTILENGUAJE: Configuración de nuestro sistema multilenguaje y el Locale

--- src/main/java/com/vaescode/springboot/app/MvcConfig.java
+++ src/main/java/com/vaescode/springboot/app/MvcConfig.java
@@ -1,14 +1,20 @@
 package com.vaescode.springboot.app;
 
+import java.util.Locale;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
-public class MvcConfig implements WebMvcConfigurer{
-	
+public class MvcConfig implements WebMvcConfigurer {
+
 	/*
 	 * private static final Logger log = LoggerFactory.getLogger(MvcConfig.class);
 	 * 
@@ -25,14 +31,37 @@ public class MvcConfig implements WebMvcConfigurer{
 	 * registry.addResourceHandler("/uploads/**")
 	 * .addResourceLocations(resourcePath); }
 	 */
-	
+
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/error_403").setViewName("error_403");
 	}
-	
+
 	@Bean
 	public static BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	// Donde guardar el parametro de nuestro locale/lenguaje Guardar en sesion el lenguaje
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(new Locale("es", "ES"));
+
+		return localeResolver;
+	}
+
+	// Interceptor para cambiar el locale para modificar el idioma de la app añ ásar el parametro por url 
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor localInterceptor = new LocaleChangeInterceptor();
+		localInterceptor.setParamName("lang");
+		return localInterceptor;
+
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
 	}
 
 }
